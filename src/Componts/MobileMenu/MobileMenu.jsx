b72eaca0efed5a97ede0122cc33cea75d129371d
bradleymatjie <bradleymatjie.job@gmail.com>
2023-07-26 16:54:40 +0200

    fully responsive

--- src/Componts/MobileMenu/MobileMenu.jsx
+++ src/Componts/MobileMenu/MobileMenu.jsx
@@ -1,17 +1,19 @@
 import { memo } from 'react';
 import './MobileMenu.scss';
+import '../../Utils/Util.css';
+import { NavLink } from 'react-router-dom';
 
 export const MobileMenu = memo(() => {
   return (
     <div className='mobileMenu'>
       <ul >
-        <li>Home</li>
-        <li>TheProblem</li>
-        <li>Our Solution</li>
-        <li>Features</li>
-        <li>Technology</li>
-        <li>Team</li>
-        <li>ContactUs</li>
+        <li><NavLink to="/">Home </NavLink></li>
+        <li><NavLink to="/TheProblem">TheProblem</NavLink></li>
+        <li><NavLink to="/solution">Our Solution</NavLink></li>
+        <li><NavLink to="/features">Features</NavLink></li>
+        <li><NavLink to="/technology">Technology</NavLink></li>
+        <li><NavLink to="/team">Team</NavLink></li>
+        <li><NavLink to="/contactus">ContactUs</NavLink></li>
       </ul>
     </div>
   )
